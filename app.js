7c436dc375f9fd7ba503eff9c5da2678cfbb805a
athamour1 <thanosmour.tk@gmail.com>
2022-02-02 15:31:56 +0200

    Last: update

--- app.js
+++ app.js
@@ -7,31 +7,60 @@ var praksi4 = null;
 
 function calc() {
   exisoseis = document.getElementById("exisoseis").value;
-  console.log(exisoseis);
   psifia = document.getElementById("psifia").value;
-  console.log(psifia);
   praksi1 = document.getElementById("praksi1").checked;
-  console.log(praksi1);
   praksi2 = document.getElementById("praksi2").checked;
-  console.log(praksi2);
   praksi3 = document.getElementById("praksi3").checked;
-  console.log(praksi3);
   praksi4 = document.getElementById("praksi4").checked;
-  console.log(praksi4);
-  alert("Τέλεια!!!\n Θα υπολογίσω τώρα");
+  prakseis = [];
+  if (praksi1) {
+    prakseis.push("+")
+  }
+  if (praksi2) {
+    prakseis.push("x")
+  }
+  if (praksi3) {
+    prakseis.push("/")
+  }
+  if (praksi4) {
+    prakseis.push("-")
+  }
+  console.log("prakseis: ", prakseis)
+  console.log("prakseis: ", prakseis.length > 0)
+  var error = document.getElementById("error");
+  var resultMessage = document.getElementById("resultMessage");
+  var results = document.getElementById("results")
+  if (prakseis.length > 0) {
+    error.innerHTML = ""
+    resultMessage.innerHTML = "Βρείτε το αποτέλεσμα"
+    alert("Τέλεια!!!\nΘα υπολογίσω τώρα");
+    var stringResult = ""
+    for (let index = 0; index < exisoseis; index += 1) {
+      let number1 = Math.floor(Math.random() * (10 * psifia))
+      let praksi = prakseis[Math.floor(Math.random() * prakseis.length)]
+      let number2 = Math.floor(Math.random() * (10 * psifia))
+      console.log(number1 + praksi + number2 + "=")
+      stringResult += "<div class='row'><div class='red col-1-ss'>" + (index+1) + ") </div><div class='col-4'>" + number1 + " " + praksi + " " + number2 + " " + "=" + "</div><div class='col-7'></div></div><div class='row'><hr style='border: 1px dashed #adadad; width: 100%;' /></div>"
+    }
+    console.log(stringResult)
+    results.innerHTML = stringResult
+  } else {
+    // error.style.color = "red"
+    error.innerHTML = "Λάθος στο είδος των πράξεων"
+    resultMessage.innerHTML =""
+  }
+
 }
 
 function clean() {
-  exisoseis = null;
-  console.log("exisoseis ", exisoseis);
-  psifia = null;
-  console.log("psifia ", psifia);
-  praksi1 = false;
-  console.log("praksi1", praksi1);
-  praksi2 = false;
-  console.log("praksi2", praksi2);
-  praksi3 = false;
-  console.log("praksi3", praksi3);
-  praksi4 = false;
-  console.log("praksi4", praksi4);
+  var error = document.getElementById("error");
+  var resultMessage = document.getElementById("resultMessage");
+  error.innerHTML = ""
+  resultMessage.innerHTML = ""
+  document.getElementById('psifia').value = '1'
+  document.getElementById('exisoseis').value = ''
+  document.getElementById("praksi1").checked = false;
+  document.getElementById("praksi2").checked = false;
+  document.getElementById("praksi3").checked = false;
+  document.getElementById("praksi4").checked = false;
 }
